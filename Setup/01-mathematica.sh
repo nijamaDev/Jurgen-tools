5bda8db397578e0c07bcaa9c3ca381e530a0d9fa
nijama <9n0101@gmail.com>
2023-04-21 15:21:49 -0500

    Mathematica auto install

--- Setup/01-mathematica.sh
+++ Setup/01-mathematica.sh
@@ -3,7 +3,20 @@
 set -e
 set -x
 
+MATHLM_SERVER=192.168.26.37
 # Make the path variable later
+INST_DIR=/nfs/.share/Install/Mathematica/
+MATH_INSTALLER=Mathematica_13.1.0_LINUX.sh
+GRID_INSTALLER=gridMathematica_13.1.0_LightweightGridManager_LINUX.sh
+OPTIONS="-- -auto -silent -selinux=y"
 
-INST_DIR=/nfs/.share/Install/Mathematica/Mathematica_13.1.0_LINUX.sh
+# Install license
+LIC_DIR=/usr/share/Mathematica/Licensing
+mkdir -p $LIC_DIR
+echo \!$MATHLM_SERVER > $LIC_DIR/mathpass
+# Install Mathematica
+$INST_DIR$MATH_INSTALLER $OPTIONS
+
+# Install gridMathematica
+$INST_DIR$GRID_INSTALLER $OPTIONS
 
